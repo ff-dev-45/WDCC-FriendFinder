1dd4d7945d41c9038f3d67437d59e5821df06746
Bob <48901347+BobTheSoftwareDeveloper@users.noreply.github.com>
2020-08-08 17:32:38 +1200

    Working map code

--- pages/hello.js
+++ pages/hello.js
@@ -1,7 +1,7 @@
 import Layout from '../components/layout'
 import { Greet, Hello } from '../components/Hello'
 import { MapContainer } from '../components/mapContainer'
-
+import ReactDOM from 'react-dom'
 
 // React Components can be very very terse
 const name = 'Henry'
@@ -13,8 +13,7 @@ export default () =>
       <Hello name={name} />
     </article>
     <div>
-      ReactDOM.render(
       <MapContainer></MapContainer>
-      )
+      <h1>Test</h1>
     </div>
   </Layout>
