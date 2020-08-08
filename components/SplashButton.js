cf66190fd791769290350de2f21900a1937ff2da
Beverley Sun <beverleysun0205@gmail.com>
2020-08-08 15:30:26 +1200

    Button for splash screen

--- components/SplashButton.js
+++ components/SplashButton.js
@@ -1,15 +1,17 @@
 export const SplashButton = () => {
+  const buttonStyle = {
+    color: 'white',
+    backgroundColor: '#79A2C2'
+
+  }
+  
   return (
     <a href='/api/login'>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">Get Started</button>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md" style={buttonStyle}>
+        Get Started
+      </button>
     </a>
-
-    
-
   )
-
 }
-
-
 
 export default SplashButton
