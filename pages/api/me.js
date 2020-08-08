ce6eb567d1a5d535f6caebeaab9dbd981c002b36
DarkMatterMatt <mattmoran2000@gmail.com>
2020-08-09 00:47:53 +1200

    store user in MongoDB

--- pages/api/me.js
+++ pages/api/me.js
@@ -1,10 +1,34 @@
 import auth0 from '../../lib/auth0'
+import nextConnect from 'next-connect'
+import mongodb from '../../middleware/database'
 
-export default async function me (req, res) {
-  try {
-    await auth0.handleProfile(req, res)
-  } catch (error) {
-    console.error(error)
-    res.status(error.status || 500).end(error.message)
+const handler = nextConnect()
+
+handler.use(mongodb)
+
+// fetch user auth data. stores user in database
+handler.get(async (req, res) => {
+  const session = await auth0.getSession(req)
+  if (session == null || session.user == null) {
+    res.json('null')
+    return
   }
-}
+  const { user } = session
+  const userid = user.sub
+
+  await req.db.collection('users').update({ userid }, {
+    $set: {
+      userid,
+      firstname: user.given_name,
+      lastname: user.family_name,
+      nickname: user.nickname,
+    },
+    $setOnInsert: {
+      friends: [],
+    },
+  }, { upsert: true })
+
+  res.json(user)
+})
+
+export default handler
