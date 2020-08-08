a632781575db4333626a10fdb42a3cb84fcd890d
DarkMatterMatt <mattmoran2000@gmail.com>
2020-08-09 01:26:45 +1200

    Support POST to /me to update own data

--- pages/api/me.js
+++ pages/api/me.js
@@ -31,4 +31,38 @@ handler.get(async (req, res) => {
   res.json(user)
 })
 
+handler.post(async (req, res) => {
+  const session = await auth0.getSession(req)
+  if (session == null || session.user == null) {
+    return res.status(401).json({ status: 'error', message: 'Unauthorized' })
+  }
+  const { user } = session
+  const userid = user.sub
+
+  if (req.body == null) {
+    return res.status(400).json({ status: 'error', message: 'No body provided' })
+  }
+
+  const { firstname, lastname, nickname, friends } = req.body
+  if ((firstname != null && typeof firstname !== 'string')
+      || (lastname != null && typeof lastname !== 'string')
+      || (nickname != null && typeof nickname !== 'string')
+      || (friends != null && (!Array.isArray(friends) || !friends.every(f => typeof f === 'string')))) {
+    return res.status(400).json({ status: 'error', message: 'Invalid parameter type' })
+  }
+
+  const safeData = {}
+  if (firstname != null) safeData.firstname = firstname
+  if (lastname  != null) safeData.lastname  = lastname
+  if (nickname  != null) safeData.nickname  = nickname
+  if (friends   != null) safeData.friends   = friends
+
+  try {
+    await req.db.collection('users').updateOne({ userid }, { $set: safeData })
+    res.json({ status: 'ok' })
+  } catch (e) {
+    res.json({ status: 'error', message: e.message })
+  }
+})
+
 export default handler
